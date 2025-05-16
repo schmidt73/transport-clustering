178f70c38d3f0a3314152ff353dba268c179f4da
Henri Schmidt <henri.schmidt@princeton.edu>
2025-05-16 10:12:52 -0400

    Try entropy regularized procedure.

--- src/convex_lrot.py
+++ src/convex_lrot.py
@@ -1,12 +1,18 @@
 import time
 import os
+import jax
 
+import jax.numpy as jnp
 import cvxpy as cp
 import numpy as np
 import gurobipy as gp
 
+from ott.geometry import geometry, pointcloud
+from ott.problems.linear import linear_problem
+from ott.solvers.linear import sinkhorn
 from loguru import logger
 from sklearn.decomposition import NMF
+from jax.experimental.sparse.linalg import lobpcg_standard as lobpcg
 
 def simplex_projection(s):
     """Projection onto the unit simplex."""
@@ -54,104 +60,13 @@ def solve_linear_ot_cvxpy(
 
     return P.value, prob.value
 
-def solve_linear_ot_cvxpy_L(
-    C: np.ndarray, 
-    g1: np.ndarray, 
-    g2: np.ndarray, 
-    L: np.ndarray,
-    solver: str = "GUROBI"
-):
-    m, n = C.shape
-    k    = L.shape[1]
-    if g1.shape != (m,) or g2.shape != (n,):
-        raise ValueError("Dimension mismatch between C, g1, and g2.")
-
-    R = cp.Variable((k, n), nonneg=True)
-
-    ones_n = np.ones(n)
-    ones_m = np.ones(m)
-
-    tol = 1e-6
-    constraints = [
-        L @ R @ ones_n <= g1 + tol,
-        L @ R @ ones_n >= g1 - tol,
-        R.T @ L.T @ ones_m <= g2 + tol,
-        R.T @ L.T @ ones_m >= g2 - tol,
-        L @ R >= 0,
-    ]
-
-    C_prime = L.T @ C
-    objective = cp.Minimize(cp.sum(cp.multiply(C_prime, R)))
-    prob = cp.Problem(objective, constraints)
-    prob.solve(solver=solver, verbose=True)
-
-    if prob.status not in ("optimal", "optimal_inaccurate"):
-        raise RuntimeError(f"Solver did not converge: status = {prob.status}")
-
-    return R.value, prob.value
-
-def solve_linear_ot_cvxpy_R(
-    C: np.ndarray,
-    g1: np.ndarray,
-    g2: np.ndarray,
-    R: np.ndarray,
-    solver: str = "GUROBI"
-):
-    m, n = C.shape
-    k    = R.shape[0]
-    if g1.shape != (m,) or g2.shape != (n,):
-        raise ValueError("Dimension mismatch between C, g1, and g2.")
-
-    L = cp.Variable((m, k), nonneg=True)
-
-    ones_n = np.ones(n)
-    ones_m = np.ones(m)
-
-    tol = 1e-6
-    constraints = [
-        L @ R @ ones_n <= g1 + tol,
-        L @ R @ ones_n >= g1 - tol,
-        R.T @ L.T @ ones_m <= g2 + tol,
-        R.T @ L.T @ ones_m >= g2 - tol,
-        L @ R >= 0,
-    ]
-
-    C_prime = C @ R.T
-    objective = cp.Minimize(cp.sum(cp.multiply(C_prime, L)))
-    prob = cp.Problem(objective, constraints)
-    prob.solve(solver=solver, verbose=True)
-
-    if prob.status not in ("optimal", "optimal_inaccurate"):
-        raise RuntimeError(f"Solver did not converge: status = {prob.status}")
-
-    return L.value, prob.value
-
-def solve_low_rank_ot_alternating_lp(
-    C: np.ndarray,
-    g1: np.ndarray,
-    g2: np.ndarray,
-    k: int,
-    iters: int = 100,
-    tolerance: float = 1e-4,
-):
-    n, m = C.shape
-    L = np.random.rand(n, k)
-    R = np.random.rand(k, m)
-    L, R, _ = sinkhorn_rescaling(L, R, g1, g2)
-
-    for _ in range(iters):
-        R, obj = solve_linear_ot_cvxpy_L(C, g1, g2, L)
-        L, obj = solve_linear_ot_cvxpy_R(C, g1, g2, R)
-
-    return L, R, obj
-
 def solve_euclidean_reg_ot_cvxpy(
     C: np.ndarray, 
     g1: np.ndarray, 
     g2: np.ndarray, 
     P_reg: np.ndarray, 
     rho: float = 0.1,
-    solver: str = "GUROBI"
+    solver: str = "MOSEK"
 ):
     m, n = C.shape
     if g1.shape != (m,) or g2.shape != (n,):
@@ -215,31 +130,77 @@ def solve_nuclear_ot(
 
     return P1, np.sum(C * P1)
 
-def solve_nuclear_ot_cvxpy(C: np.ndarray, g1: np.ndarray, g2: np.ndarray, k: int,
-                     gamma: float, solver: str = "MOSEK"):
-    m, n = C.shape
-    if g1.shape != (m,) or g2.shape != (n,):
-        raise ValueError("Dimension mismatch between C, g1, and g2.")
+def entropy_regularized_nuclear_projection(
+    C,
+    P1: jnp.ndarray,
+    P2: jnp.ndarray,
+    D: jnp.ndarray,
+    nuclear_norm_bound : float,
+    iterations: int = 1000,
+):
+    # MAJOR ISSUE: doesn't preserve feasability
+    def loss(P2, P1, D):
+        return jnp.sum(D * P2) + jnp.sum(jax.scipy.special.rel_entr(P1, P2))
+    loss_grad = jax.value_and_grad(loss)
+    # use franke wolfe or nuclear norm ball
+    for i in range(iterations):
+        obj, G = loss_grad(P2, P1, D)
+        step_size = 1e-2
+        #print(P1)
+        print(f"Q-step Objective Cost: {obj}")
+        # G = D - jnp.where(P2 != 0, P1 / P2, 0)  # gradient of the objective with safeguard for division by zero
+        U, S, V = jnp.linalg.svd(G)             # TODO: replace with lobpcg to find only 1 singular vector
+        #print(G)
+        direction = -nuclear_norm_bound * U[:, 0] @ V[0, :]
+        P2 = (1 - step_size) * P2 + step_size * direction
+        print(f"Linear Objective Cost: {(C * P2).sum()}")
+        #1/ 0
+    return P2
 
-    P = cp.Variable((m, n), nonneg=True)
+#@jax.jit
+def sink_helper(
+    C, g1, g2, epsilon, min_iterations, max_iterations
+):
+    geom = geometry.Geometry(cost_matrix=C, epsilon=epsilon)
+    prob = linear_problem.LinearProblem(geom, g1, g2)
+    solver = sinkhorn.Sinkhorn(min_iterations=min_iterations, max_iterations=max_iterations)
+    return solver(prob).matrix
 
-    ones_n = np.ones(n)
-    ones_m = np.ones(m)
+def solve_nuclear_ot_sinkhorn(
+    C: jnp.ndarray,
+    g1: jnp.ndarray,
+    g2: jnp.ndarray,
+    k: jnp.int32, 
+    gamma: jnp.float32, 
+    max_iter: jnp.int32 = 100, 
+    tolerance: jnp.float32 = 1e-4, 
+    epsilon: jnp.float32 = 1e-3,
+    verbose: bool = False
+):
+    P2, D = jnp.ones_like(C), jnp.zeros_like(C)
+    iteration = 0
+    while iteration < max_iter:
+        start_time_entropy_ot = time.time()
+        P1 = sink_helper(C - D - jnp.log(P2), g1, g2, epsilon, min_iterations=0, max_iterations=500)
+        end_time_entropy_ot = time.time()
 
-    constraints = [
-        P @ ones_n == g1,
-        P.T @ ones_m == g2,
-        cp.norm(P, "nuc") <= gamma * k,
-    ]
+        if verbose:
+            logger.info(f"Time for Entropy Regularized OT: {end_time_entropy_ot - start_time_entropy_ot}")
+            logger.info(f"Linear Objective (P1): {jnp.sum(C * P1)}")
 
-    objective = cp.Minimize(cp.sum(cp.multiply(C, P)))
-    prob = cp.Problem(objective, constraints)
-    prob.solve(solver=solver, verbose=True)
+        start_time_nuc_proj = time.time()
+        P2 = 1 / (C.shape[0] * C.shape[1]) * jnp.ones_like(C)
+        P2 = entropy_regularized_nuclear_projection(C, P1, P2, D, gamma * k, iterations=1000)
+        end_time_nuc_proj = time.time()
 
-    if prob.status not in ("optimal", "optimal_inaccurate"):
-        raise RuntimeError(f"Solver did not converge: status = {prob.status}")
+        if verbose:
+            logger.info(f"Time for Nuclear Projection: {end_time_nuc_proj - start_time_nuc_proj}")
+            logger.info(f"Linear Objective (P2): {jnp.sum(C * P2)}")
 
-    return P.value, prob.value
+        R  = P1 - P2
+        D  = D + R
+
+        iteration += 1
 
 def sinkhorn_rescaling(L, R, g1, g2, max_iter=1000, tol=1e-12):
     rescaling_rows = True
@@ -267,7 +228,7 @@ def sinkhorn_rescaling(L, R, g1, g2, max_iter=1000, tol=1e-12):
     return L, R, singular_values
 
 def nonnegative_rounding(P, g1, g2, k, seed=0):
-    model = NMF(n_components=k, init='random', random_state=seed, max_iter=1000)
+    model = NMF(n_components=k, init='random', random_state=seed, max_iter=10000, solver='mu', beta_loss='frobenius')
     W = model.fit_transform(P)
     H = model.components_
     L_round, R_round, singular_values = sinkhorn_rescaling(W, H, g1, g2)
